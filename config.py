6f56c7e428436d307aa9736d7e80851a32321804
Saujanya-Grampurohit <saujanyagrampurohit04@gmail.com>
2025-06-27 23:10:17 +0530

    Fixed: corrected DB password

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
+# config.py
 import mysql.connector
+import os
 from mysql.connector import errorcode
 
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host='yamabiko.proxy.rlwy.net',
-            port=35892,
-            user='root',
-            password='abcd1234',  # 🔁 Replace with your actual password
-            database='railway'
+            host=os.environ.get("MYSQLHOST", "mysql.railway.internal"),
+            user=os.environ.get("MYSQLUSER", "root"),
+            password=os.environ.get("MYSQLPASSWORD", ""),
+            database=os.environ.get("MYSQLDATABASE", "railway"),
+            port=int(os.environ.get("MYSQLPORT", 3306))
         )
         return conn
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             raise RuntimeError("DB authentication error")
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            raise RuntimeError("DB does not exist")
+            raise RuntimeError("Database does not exist")
         else:
             raise
